9b7bd04d11f63acc9b4331d2a9d35b718b0bb050
csf <chenshifang@jiuxunmu.com>
2021-10-21 18:25:20 +0800

    123

--- src/Auth/EhrAuthRpc.php
+++ src/Auth/EhrAuthRpc.php
@@ -15,6 +15,8 @@ class EhrAuthRpc
 {
     public $client;
     public $allInfos = [];
+    public $resultInfos = [];
+    public $editorInfos = [];
 
     public function addInfos(&$error, $account, $password, $name, $identify, $sex, $phone, $entry_time,
                              $address = null, $img = null, $describe = null, $editor_id = null, $details = null)
@@ -44,49 +46,49 @@ class EhrAuthRpc
 
     public function sendInfo()
     {
-
+        $client = new Client(config('ehr.service') . 'archive/info', false);
+        $res = $client->import(json_encode($this->allInfos), new InvokeSettings(['mode' => ResultMode::Normal]));
+        $this->resultInfos = [
+            'success' => $res['success'],
+            'fail' => $res['fail'],
+        ];
+        return $this->resultInfos;
+//        Log::info('result:' . $send, $res);
+//        foreach ($res['success'] as $item) {
+//
+//            UserInfo::where('id', $item['userinfo_id'])->update([
+//                'sc_id' => $item['sc_id'],
+//            ]);
+//        }
+//        foreach ($res['fail'] as $item) {
+//            if ($item['type'] == 'exists') {
+//                UserInfo::where('id', $item['userinfo_id'])->update([
+//                    'sc_id' => $item['sc_id'],
+//                ]);
+//            }
+//        }
+//        $infos = UserInfo::with(['editor:id,sc_id'])->where('id', '!=', 1)
+//            ->get(['id', 'editor_id', 'username', 'identityNumber', 'sc_id']);
+//        $res = $client->updateEditor(json_encode($infos->toArray()), new InvokeSettings(['mode' => ResultMode::Normal]));
+//        Log::info('update result', $res);
     }
 
-    public function init()
+    public function clearInfos()
     {
-        $client = new Client(config('ehr.url') . '/archive/info', false);
-        $allCount = UserInfo::whereNull('sc_id')->count();
-        $success = $fail = 0;
-        $send = 0;
-        $users = User::get()->makeVisible(['id', 'password', 'name', 'created_at', 'updated_at', 'deleted_at', 'userinfo_id']);
-        $infos = UserInfo::with(['editor:id,sc_id,identityNumber'])->whereNull('sc_id')->where('id', '!=', 1)
-            ->get();
-        $infos->map(function ($item) use ($users) {
-            $item->account = $users->where('userinfo_id', $item->id)->first();
-            if (!is_null($item->account)) $item->account = $item->account->toArray();
-        });
-        Log::info('infos' . $success, $infos->toArray());
-        $res = $client->import(json_encode($infos->toArray()), new InvokeSettings(['mode' => ResultMode::Normal]));
-
-        echo json_encode($res) . "\r\n";
-        Log::info('result:' . $send, $res);
-
-        foreach ($res['success'] as $item) {
-            UserInfo::where('id', $item['userinfo_id'])->update([
-                'sc_id' => $item['sc_id'],
-            ]);
-        }
-        foreach ($res['fail'] as $item) {
-            if ($item['type'] == 'exists') {
-                UserInfo::where('id', $item['userinfo_id'])->update([
-                    'sc_id' => $item['sc_id'],
-                ]);
-            }
-        }
-        $infos = UserInfo::with(['editor:id,sc_id'])->where('id', '!=', 1)
-            ->get(['id', 'editor_id', 'username', 'identityNumber', 'sc_id']);
-        $res = $client->updateEditor(json_encode($infos->toArray()), new InvokeSettings(['mode' => ResultMode::Normal]));
-        Log::info('update result', $res);
+        $this->allInfos = [];
+        $this->editorInfos = [];
     }
 
-    public function addInfo()
+    public function addEditorInfo($identity, $editor_sc_id)
+    {
+        $this->editorInfos[] = [
+            'identity' => $identity,
+            'editor_id' => $editor_sc_id,
+        ];
+    }
+
+    public function updateEditor()
     {
 
     }
-
 }
