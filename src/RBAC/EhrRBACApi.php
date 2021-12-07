3ba482152ba7c410a444c10bd4d800c5ad423ef9
csf <chenshifang@jiuxunmu.com>
2021-12-07 11:36:27 +0800

    123

--- src/RBAC/EhrRBACApi.php
+++ src/RBAC/EhrRBACApi.php
@@ -15,7 +15,7 @@ class EhrRBACApi
         return self::canMenu($error, $tokenInfos, $menu_name, config('ehr.app_id'), $department_id, $resource);
     }
 
-    public static function canMenu(&$error, JxmEhrTokenInfos $tokenInfos, $menu_name, $app_id = 0, $department_id = 0, $resource = null)
+    private static function canMenu(&$error, JxmEhrTokenInfos $tokenInfos, $menu_name, $app_id = 0, $department_id = 0, $resource = null)
     {
         $result = JxmEhrAccessHelper::api($error, 'rbac/rbac/canMenu', $tokenInfos, [
             'app_id' => $app_id,
@@ -31,12 +31,13 @@ class EhrRBACApi
         return true;
     }
 
-    public static function operate(&$error, JxmEhrTokenInfos $tokenInfos, $module_name, $operate, $department_id = 0)
+    public static function operate(&$error, JxmEhrTokenInfos $tokenInfos, $module_name, $operate, $department_id = null)
     {
         return self::canOperate($error, $tokenInfos, $module_name, $operate, config('ehr.app_id'), $department_id);
     }
 
-    public static function canOperate(&$error, JxmEhrTokenInfos $tokenInfos, $module_name, $operate, $app_id = 0, $department_id = 0)
+    private static function canOperate(&$error, JxmEhrTokenInfos $tokenInfos, $module_name, $operate, $app_id,
+                                       $department_id = null)
     {
         $result = JxmEhrAccessHelper::api($error, 'rbac/rbac/canOperate', $tokenInfos, [
             'app_id' => $app_id,
@@ -52,9 +53,47 @@ class EhrRBACApi
         return true;
     }
 
-    public static function can(&$error, JxmEhrTokenInfos $tokenInfos, $department_id, $permission_id)
+    public static function menuUrl(&$error, JxmEhrTokenInfos $tokenInfos, $url, $app_id = null, $resource = null,
+                                   $bg_id = null, $department_id = null)
+    {
+        return self::canMenuUrl($error, $tokenInfos, $url, $app_id ?: config('ehr.app_id'),
+            $resource, $bg_id ?: config('ehr.bg_id'), $department_id);
+    }
+
+    /**
+     * Notes: 根据url判断菜单权限
+     * User: harden - 2021/12/7 上午10:26
+     * @param $error
+     * @param JxmEhrTokenInfos $tokenInfos
+     * @param $url
+     * @param $app_id
+     * @param $resource
+     * @param $bg_id
+     * @param $department_id
+     * @return bool
+     */
+    private static function canMenuUrl(&$error, JxmEhrTokenInfos $tokenInfos, $url, $app_id, $resource,
+                                       $bg_id, $department_id)
+    {
+        $result = JxmEhrAccessHelper::api($error, 'rbac/rbac/canMenuUrl', $tokenInfos, [
+            'app_id' => $app_id,
+            'url' => $url,
+            'bg_id' => $bg_id,
+            'resource' => $resource,
+            'department_id' => $department_id,
+        ]);
+        if ($error) return false;
+        if ($result['code'] != 0) {
+            $error = $result['msg'];
+            return false;
+        }
+        return true;
+    }
+
+    public static function can(&$error, JxmEhrTokenInfos $tokenInfos, $permission_id, $bg_id = null, $department_id = null)
     {
         $result = JxmEhrAccessHelper::api($error, 'rbac/rbac/can', $tokenInfos, [
+            'bg_id' => $bg_id ?: config('ehr.bg_id'),
             'department_id' => $department_id,
             'permission_id' => $permission_id,
         ]);
@@ -73,7 +112,7 @@ class EhrRBACApi
         $result = JxmEhrAccessHelper::api($error, 'rbac/role/own', $tokenInfos, [
             'page' => 1,
             'limit' => 200,
-            'bg_id' => $bg_id,
+            'bg_id' => $bg_id ?: config('ehr.bg_id'),
         ]);
         if ($error) return null;
         if ($result['code'] != 0) {
