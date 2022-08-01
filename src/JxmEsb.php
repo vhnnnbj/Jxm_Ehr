d1cab35903dfc54f0f8acbd3d70c291574335e02
csf <chenshifang@jiuxunmu.com>
2022-08-01 17:13:23 +0800

    update esb

--- src/JxmEsb.php
+++ src/JxmEsb.php
@@ -6,7 +6,6 @@ namespace Jxm\Ehr;
 
 use GuzzleHttp\Client;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
 
 class JxmEsb
 {
@@ -29,29 +28,63 @@ class JxmEsb
         return $result['msgs'];
     }
 
-    public static function set(&$error, $module, $type, $subtype, $infos = null, $editor_id = null, $app_id = null)
+    public static function set(&$error, $name, $value, $route = null,
+                               $infos = null, $editor_id = null, $app_id = null)
     {
-//        'app_id' => 'required',
-//            'module' => 'required|string',
-//            'type' => 'required|string',
-//            'subtype' => 'required|string',
-//            'infos' => 'nullable|json',
-//            'editor_id' => 'required|integer',
         $app_id = config('ehr.app_id', '');
         if (!$app_id) {
             $error = '请正确配置APPID!';
             return false;
         }
         $params = [
-            'app_id' => config('ehr.app_id', ''),
-            'module' => $module,
-            'type' => $type,
-            'subtype' => $subtype,
+            'app_id' => $app_id,
+            'route' => $route,
+            'name' => $name,
+            'value' => $value,
             'infos' => $infos,
-            'editor_id' => $editor ? 0,
+            'editor_id' => $editor ?: 0,
         ];
         self::postApi($error, config('ehr.esb') . 'esb/set', $params);
         return $error ? false : true;
+    }
+
+    /**
+     * Notes: $msg为空时获取错误信息，不为空时设置错误信息
+     * User: harden - 2022/8/1 下午5:08
+     * @param $track_id
+     * @param null $msg
+     * @param null $obj
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function errMsg($track_id, $msg = null, $obj = null)
+    {
+        $params['track_id'] = $track_id;
+        if ($msg) {
+            $params['msg'] = $msg;
+            $params['obj'] = $obj;
+        }
+        $client = new Client(['http_errors' => false]);
+        $response = $client->request('POST', config('ehr.esb') . $url, [
+            'headers' => [
+                'X-Requested-With' => 'XMLHttpRequest',
+            ],
+            'form_params' => $params,
+        ]);
+        if ($msg) {
+            return true;
+        } else {
+            if ($response->getStatusCode() == 200) {
+                $result = json_decode($response->getBody()->getContents()['err'], true);
+            } else {
+                $result = [
+                    'obj' => null,
+                    'msg' => '未知的错误信息！',
+                    'code' => 500,
+                ];
+            }
+            return $result;
+        }
     }
 
     public static function postApi(&$error, $url, $params = [],
