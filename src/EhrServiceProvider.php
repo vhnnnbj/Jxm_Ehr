60c9fb7c969634ad672c70d1a7dcf21330566a76
chenshifang@jiuxunmu.com <chenshifang@jiuxunmu.com>
2022-11-04 11:43:20 +0800

    add validate extends

--- src/EhrServiceProvider.php
+++ src/EhrServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Jxm\Ehr;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use Jxm\Ehr\Model\BgDepartment;
+use Jxm\Ehr\Model\UserInfo;
 
 class EhrServiceProvider extends ServiceProvider
 {
@@ -28,5 +31,20 @@ class EhrServiceProvider extends ServiceProvider
         ], 'ehr_config');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 //        $this->loadRoutesFrom(__DIR__ . '../route/route.php');
+
+        Validator::extend('iam_user_exists', function ($attribute, $value,
+                                                       $parameters, $validator) {
+            if (sizeof($parameters[0]) == 0) {
+                $query = UserInfo::where('id', $value);
+            } else {
+                $query = UserInfo::where($parameters[0], $value);
+            }
+            return $query->exists();
+        }, '未查询到指定用户信息。');
+        Validator::extend('iam_department_exists', function ($attribute, $value,
+                                                             $parameters, $validator) {
+            $query = BgDepartment::where('id', $value);
+            return $query->exists();
+        }, '未查询到指定部门信息。');
     }
 }
